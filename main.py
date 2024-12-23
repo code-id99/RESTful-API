4bd25e816a43c9f5a4153b1d1e8fb80bff1cc270
Wanga Thagwana <levonolenovo99@gmail.com>
2024-12-23 08:20:53 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.roote("/get-user/<user_id>")
+def get_user(user_id):
+  user_data = {
+    "user_id": user_id,
+    "name": "Wanga Mukona",
+    "email": "john.doe@example.com"
+  }
+
+  extra = request.args.get("extra")
+  if extra:
+    user_data["extra"] = extra
+
+  return jsonify(user_data), 200
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+  data = request.get_json()
+
+  return jsonify(data), 201
+
+
+if__name__ == "__main__":
+  app.run(debug=True)
